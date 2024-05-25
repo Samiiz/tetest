925767aa94afa1c4f957325cf80fc96e1149d750
Samiiz <skboss159@gmail.com>
2024-05-25 12:30:15 +0900

    chore: job test

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,4 @@
 
 aerich init -t app.configs.database_settings.TORTOISE_ORM
 aerich init-db
-poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+poetry run uvicorn main:app --host 0.0.0.0 --port 80 --reload
